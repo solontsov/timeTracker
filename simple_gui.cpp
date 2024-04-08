7129a9e977118ff54a5d90fc2507bafa8213608f
Oleksandr Solontsov <solontsov@gmail.com>
2024-04-08 12:08:51 +0100

    switch

--- simple_gui.cpp
+++ simple_gui.cpp
@@ -1,36 +1,59 @@
-#include <Windows.h>
+#include <windows.h>
+
+BOOL trackerIsOn = FALSE;
+RECT rect = {5, 5, 55, 35};
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
-        case WM_PAINT:
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        const COLORREF rgbGreen = 0x0000FF00;
+        const COLORREF rgbBackground = GetSysColor(COLOR_WINDOW);
+        HBRUSH hbr;
+
+        if (trackerIsOn)
         {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            TextOutW(hdc, 10, 10, L"Hello, Windows desktop!", 22); // Use TextOutW for wide-character strings
-            EndPaint(hwnd, &ps);
-            break;
+            hbr = CreateSolidBrush(rgbGreen);
         }
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProcW(hwnd, msg, wParam, lParam); // Use DefWindowProcW
+        else
+        {
+            hbr = CreateSolidBrush(rgbBackground);
+        }
+        FillRect(hdc, &rect, hbr);
+        DeleteObject(hbr);
+
+        TextOutW(hdc, 10, 10, L"321 Hello!", 3); // Use TextOutW for wide-character strings
+        EndPaint(hwnd, &ps);
+        break;
+    }
+    case WM_LBUTTONDOWN:
+        trackerIsOn = !trackerIsOn;
+        InvalidateRect(hwnd, &rect, TRUE);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hwnd, msg, wParam, lParam); // Use DefWindowProcW
     }
     return 0;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    WNDCLASSW wc = { 0 }; // Use WNDCLASSW for wide-character strings
+    WNDCLASSW wc = {0}; // Use WNDCLASSW for wide-character strings
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = L"MyWindowClass"; // Prefix with L for wide-character string
-    RegisterClassW(&wc); // Use RegisterClassW
+    RegisterClassW(&wc);                 // Use RegisterClassW
 
-    HWND hwnd = CreateWindowW(L"MyWindowClass", L"Hello, Windows desktop!", WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, NULL, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowW(L"MyWindowClass", L"Hello, Windows desktop123!", WS_OVERLAPPEDWINDOW,
+                              CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, NULL, NULL, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
 
